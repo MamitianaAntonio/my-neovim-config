2422df7e6d825d86c6afbcf47748d847549a8edb
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-02-28 22:04:38 +0300

    feat(lualine&colorscheme) : add rusty for colorscheme and install lualine via lazy

--- init.lua
+++ init.lua
@@ -795,6 +795,7 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    enabled = false,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
@@ -806,7 +807,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
